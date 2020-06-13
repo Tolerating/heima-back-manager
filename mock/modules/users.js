a86ea59c512ae8fbea98cfd463e9b3ff86d3f4b2
刘凯 <354378485@qq.com>
2020-06-14 00:27:57 +0800

    更改状态和编辑用户

--- mock/modules/users.js
+++ mock/modules/users.js
@@ -27,7 +27,8 @@ import Mock from 'mockjs'
 //         })
 //     }
 // }
-Mock.mock(new RegExp("/users"+"/\?"),"get",(opts)=>{
+/* 获取用户 */
+Mock.mock(new RegExp("/users"),"get",(opts)=>{
     const {url} = opts;
     let data = {};
     url.split("?")[1].split("&").forEach((item)=>{
@@ -58,4 +59,77 @@ Mock.mock(new RegExp("/users"+"/\?"),"get",(opts)=>{
             "status": 200
         }
     });
+});
+
+/* 添加用户 */
+Mock.mock(new RegExp("/users"),"post",(opts)=>{
+    const {username,email,mobile} = JSON.parse(opts.body);
+    console.log(JSON.parse(opts.body));
+    return Mock.mock({
+        "data": {
+            "id": 28,
+            "username": username,
+            "mobile": mobile,
+            "type": 1,
+            "openid": "",
+            "email": email,
+            "create_time": "@datetime()",
+            "modify_time": null,
+            "is_delete": false,
+            "is_active": false
+        },
+        "meta": {
+            "msg": "用户创建成功",
+            "status": 201
+        }
+    })
+});
+
+/* 删除用户 */
+Mock.mock(new RegExp("/users"),"delete",(opts)=>{
+    console.warn("删除用户",opts);
+    return {
+        "data": null,
+        "meta": {
+            "msg": "删除成功",
+            "status": 200
+        }
+    }
+});
+
+/* 编辑用户 */
+Mock.mock(new RegExp("/users/.$"),"put",(opts)=>{
+    console.warn("编辑用户",opts);
+    return {
+        "data": {
+            "id": 503,
+            "username": "admin3",
+            "role_id": 0,
+            "mobile": "111",
+            "email": "123@123.com"
+        },
+        "meta": {
+            "msg": "更新成功",
+            "status": 200
+        }
+    }
+});
+
+/* 更改用户状态 */
+Mock.mock(new RegExp("/users"+"/."+"/.$"),"put",(opts)=>{
+    console.warn("编辑用户",opts);
+    return {
+        "data": {
+            "id": 566,
+            "rid": 30,
+            "username": "admin",
+            "mobile": "123456",
+            "email": "bb@itcast.com",
+            "mg_state": 0
+          },
+          "meta": {
+            "msg": "设置状态成功",
+            "status": 200
+          }
+    }
 });
--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -12,9 +12,10 @@
             <el-input placeholder="请输入内容" clearable @clear="loadUserList" v-model="query" class="inputSearch">
                 <el-button @click="searchUser" slot="append" icon="el-icon-search"></el-button>
             </el-input>
-            <el-button type="success">添加用户</el-button>
+            <el-button type="success" @click="showAddUserDia">添加用户</el-button>
         </el-col>
     </el-row>
+
     <!-- 3.表格 -->
     <el-table :data="userlist" style="width: 100%">
         <el-table-column fixed label="#" type="index" width="60">
@@ -40,27 +41,63 @@
         </el-table-column>
         <el-table-column prop="mg_state" label="用状态">
             <template slot-scope="scope">
-                <el-switch v-model="scope.row.mg_state"></el-switch>
+                <el-switch v-model="scope.row.mg_state" @change="changeMsgState(scope.row)"></el-switch>
             </template>
         </el-table-column>
         <el-table-column label="操作">
             <template slot-scope="scope">
-                <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
-                <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+                <el-button size="mini" plain type="primary" icon="el-icon-edit" circle @click="showEditUserDia(scope.row)"></el-button>
+                <el-button size="mini" plain type="danger" icon="el-icon-delete" @click="showDeleteUserMsg(scope.row.id)" circle></el-button>
                 <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
             </template>
         </el-table-column>
     </el-table>
     <!-- 4.分页 -->
-    <el-pagination 
-      @size-change="handleSizeChange" 
-      @current-change="handleCurrentChange" 
-      :current-page="pagenum | number" 
-      :page-sizes="[2, 4, 6, 8]" 
-      :page-size="2" 
-      layout="total, sizes, prev, pager, next, jumper" 
-      :total="total">
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum | number" :page-sizes="[2, 4, 6, 8]" :page-size="2" layout="total, sizes, prev, pager, next, jumper" :total="total">
     </el-pagination>
+
+    <!-- 添加用户的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+        <el-form ref="ruleAddForm" :rules="addRules" :model="addForm">
+            <el-form-item label="用户名" label-width="100px" prop="username">
+                <el-input v-model="addForm.username" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" label-width="100px" prop="password">
+                <el-input v-model="addForm.password" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" label-width="100px">
+                <el-input v-model="addForm.email" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="电话" label-width="100px">
+                <el-input v-model="addForm.mobile" auto-complete="off"></el-input>
+            </el-form-item>
+
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+            <el-button type="primary" @click="addUser">确 定</el-button>
+        </div>
+    </el-dialog>
+
+    <!-- 编辑用户的对话框 -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleEdit">
+        <el-form :model="addForm">
+            <el-form-item label="用户名" label-width="100px" prop="username">
+                <el-input v-model="addForm.username" disabled auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" label-width="100px">
+                <el-input v-model="addForm.email" auto-complete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="电话" label-width="100px">
+                <el-input v-model="addForm.mobile" auto-complete="off"></el-input>
+            </el-form-item>
+
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+            <el-button type="primary" @click="editUser">确 定</el-button>
+        </div>
+    </el-dialog>
 </el-card>
 </template>
 
@@ -68,66 +105,183 @@
 export default {
     name: 'users',
     data() {
-      return {
-        query: '',
-        total: -1,
-        pagenum: "1",
-        pagesize: 2,
-        userlist: []
-      }
+        return {
+            query: '',
+            total: -1,
+            pagenum: "1",
+            pagesize: 2,
+            userlist: [],
+            dialogFormVisibleAdd: false,
+            dialogFormVisibleEdit:false,
+            addForm: {
+                username: "",
+                password: "",
+                email: "",
+                mobile: "",
+            },
+            addRules: {
+                username: [{
+                    required: true,
+                    message: '用户名不能为空',
+                    trigger: 'blur'
+                }],
+                password: [{
+                    required: true,
+                    message: '密码不能为空',
+                    trigger: 'blur'
+                }]
+            }
+        }
     },
     created() {
         this.getUserList();
     },
     methods: {
-      async getUserList() {
-        const res = await this.$http.get('users',{
-          params:{
-            "query":this.query,
-            "pagenum":this.pagenum,
-            "pagesize":this.pagesize
+        // 获取用户
+        async getUserList() {
+            const res = await this.$http.get('users', {
+                params: {
+                    "query": this.query,
+                    "pagenum": this.pagenum,
+                    "pagesize": this.pagesize
+                }
+            });
+            const {
+                meta: {
+                    status,
+                    msg
+                },
+                data: {
+                    users,
+                    total,
+                    pagenum
+                }
+            } = res.data;
+            console.log(res);
+            if (status === 200) {
+                this.userlist = users;
+                this.total = total;
+                this.pagenum = pagenum;
+                this.$message.success(msg);
+            } else {
+                this.$message.warning(msg);
+            }
+        },
+        // 分页组件,每页显示总数变化
+        handleSizeChange(val) {
+            console.log(`每页 ${val} 条`);
+            this.pagesize = val;
+            this.pagenum = 1;
+            this.getUserList();
+        },
+        // 分页组件,当前页变化
+        handleCurrentChange(val) {
+            console.log(`当前页: ${val}`);
+            this.pagenum = val;
+            this.getUserList();
+        },
+        // 搜索用户
+        searchUser() {
+            this.pagenum = 1;
+            this.getUserList();
+        },
+        // 搜索框清空后,重新获取用户
+        loadUserList() {
+            this.pagenum = 1;
+            this.getUserList();
+        },
+        // 添加用户 - 打开对话框
+        showAddUserDia(){
+          this.addForm = {};
+          this.dialogFormVisibleAdd = true;
+        },
+        // 添加用户
+        addUser() {
+            this.dialogFormVisibleAdd = false;
+            console.log(this.$refs);
+            this.$refs["ruleAddForm"].validate(async (valid) => {
+                if (valid) {
+                    const res = await this.$http.post("users", this.addForm);
+                    console.log(res);
+                    const {
+                        meta: {
+                            msg,
+                            status
+                        },
+                        data
+                    } = res.data;
+                    if (status === 201) {
+                        this.$message.success(msg);
+                        // 更新视图(开发环境下模拟数据随机生成,看不到添加的数据)
+                        // this.getUserList();
+                        // 清空文本框
+                        // this.addForm = {};
+                        // 另一种清空方法
+                        for (const key in this.addForm) {
+                            if (this.addForm.hasOwnProperty(key)) {
+                                this.addForm[key] = "";
+                            }
+                        }
+                    } else {
+                        this.$message.warning(msg);
+                    }
+                }
+            });
+        },
+        // 删除用户,打开对话框
+        showDeleteUserMsg(id) {
+            this.$confirm('删除用户?, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(async () => {
+                const res = await this.$http.delete(`users/${id}`);
+                console.log(res);
+                if (res.data.meta.status === 200) {
+                  this.pagenum = 1;
+                  this.getUserList();
+                  this.$message({
+                      type: 'success',
+                      message: '删除成功!'
+                  });
+                }
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });
+            });
+        },
+        // 编辑用户 - 显示对话框
+        showEditUserDia(user){
+          console.log(user);
+          this.addForm = user;
+          this.dialogFormVisibleEdit = true;
+        },
+        // 编辑用户,发送请求
+        async editUser(){
+          this.dialogFormVisibleEdit = false;
+          const res = await this.$http.put(`users/${this.addForm.id}`);
+          console.log(res);
+          const {meta:{msg,status}} = res.data;
+          if (status === 200) {
+            this.pagenum = 1;
+            this.getUserList();
+            this.$message.success(msg);
+          }else{
+            this.$message.warning(msg);
           }
-        });
-        const {
-          meta: {
-            status,
-            msg
-          },
-          data: {
-            users,
-            total,
-            pagenum
+        },
+        // 更改用户状态
+        async changeMsgState(user){
+          const res = await this.$http.put(`users/${user.id}/${user.type}`);
+          const {meta:{msg,status}} = res.data;
+          if (status === 200) {
+            this.$message.success(msg);
+          }else{
+            this.$message.warning(msg);
           }
-        } = res.data;
-        console.log(res);
-        if (status === 200) {
-          this.userlist = users;
-          this.total = total;
-          this.pagenum = pagenum;
-          this.$message.success(msg);
-        } else {
-          this.$message.warning(msg);
-        }
-      },
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-        this.pagesize = val;
-        this.pagenum = 1;
-        this.getUserList();
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-        this.pagenum = val;
-        this.getUserList();
-      },
-      searchUser(){
-        this.pagenum = 1;
-        this.getUserList();
-      },
-      loadUserList(){
-        this.pagenum = 1;
-        this.getUserList();
-      },
+        },
 
     }
 }
